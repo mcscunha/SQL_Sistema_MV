33284bb6059767d1e7fc483b7b4c36589e016269
MuriloCunha <mscunha@yahoo.com.br>
2019-05-16 13:52:14 -0300

    VDIC_TEMPO_PACIENTE - Acrescimo de coluna "PO"

--- VDIC_TEMPO_PACIENTE.sql
+++ VDIC_TEMPO_PACIENTE.sql
@@ -23,168 +23,177 @@ CREATE OR REPLACE FORCE EDITIONABLE VIEW "DBAMV"."VDIC_TEMPO_PACIENTE" (
     "DS_ORI_ATE"
 ) AS
     SELECT DISTINCT
-        dt_atendimento AS data,
-        atendime.cd_atendimento,
-        MIN(TO_CHAR(vdic_recepcao_agenda.hr_agenda, 'hh24:mi')) AS hr_agenda,
-        TO_CHAR(hr_atendimento,'hh24:mi') hr_atend,
-        TO_CHAR(hr_enf, 'hh24:mi') hr_ato_enf
-    ,
-        TO_CHAR(hr_evo_enf, 'hh24:mi') hr_evolucao,
-        mod( trunc((MAX(hr_evo_enf - hr_enf)) * 24), 24)
-        || ' : '
-        || mod(trunc((MAX(hr_evo_enf - hr_enf)) * 1440), 60 ) AS tp_ato_enf,
-        TO_CHAR(dh_processo, 'hh24:mi') hr_ato_med,
-        mod(trunc((MAX(dh_processo - hr_evo_enf)) * 24), 24)
-        || ' : '
-        || mod(trunc((MAX(dh_processo - hr_evo_enf)) * 1440), 60) AS tp_enf_med,
-        TO_CHAR(hr_alta, 'hh24:mi') hr_alta,
-        mod(trunc((MAX(hr_alta - dh_processo)) * 24 ), 24)
-        || ' : '
-        || mod(trunc((MAX(hr_alta - dh_processo)) * 1440), 60) AS tp_paciente_sala,
-        CASE
-            WHEN hr_alta IS NOT NULL THEN
-                mod(trunc((hr_alta - hr_atendimento) * 24), 24)
-                || ' : '
-                || mod(trunc((hr_alta - hr_atendimento) * 1440), 60)
-            WHEN hr_alta IS NULL THEN
-                mod(trunc((SYSDATE - hr_atendimento) * 24), 24)
-                || ' : '
-                || mod(trunc((SYSDATE - hr_atendimento) * 1440), 60)
-        END tp_total,
-        paciente.nm_paciente,
-        trunc(TO_CHAR(SYSDATE - paciente.dt_nascimento) / 365.25) AS idade,
-        nm_prestador,
-        nm_convenio,
-        tp_convenio,
-        -- 15/05/2019 - MuriloCunha - Acrescentado TIPO "PO".
-        -- Como "PO" é um ITEM DE AGENDAMENTO, nao dava para utilizar somente a coluna TP_AGENDA,
-        -- entao, adicionou-se outra coluna para comparacao de TIPO DE AGENDA
-        --
-        DECODE(tp_agenda, 'A', 'Ambulatorio', 'E', 'Externo') as TIPO,
-        /*
-        case
-            when ((vdic_recepcao_agenda.tp_agenda in ('A', 'I')) and (cd_item_agendamento in (19, 20, 21, 68, 69, 112, 113, 70, 86))) then 'PO'
-            when (vdic_recepcao_agenda.tp_agenda = 'I') then 'EXAME'
-            when (vdic_recepcao_agenda.tp_agenda = 'A') then 'AMBULATORIO'
-            else 'DESCONHECIDO'
-        end as TIPO,
-        -- FIM - 15/05/2019 - MuriloCunha
-        */
-        CASE
-            WHEN evo_enf.cd_evo_enf IS NOT NULL
-                 AND hr_alta IS NULL THEN
-                'Liberado Atendimento'
-            WHEN hr_enf IS NOT NULL
-                 AND evo_enf.cd_evo_enf IS NULL THEN
-                'Em Atendimento Enfermagem'
-            WHEN evo_enf.cd_evo_enf IS NOT NULL
-                 AND dh_processo IS NOT NULL THEN
-                'Em Atendimento Medico'
-            WHEN evo_enf.cd_evo_enf IS NULL
-                 AND hr_alta IS NULL THEN
-                'Aguardando Atendimento'
-            WHEN evo_enf.cd_evo_enf IS NULL
-                 AND hr_alta IS NOT NULL THEN
-                'Liberacao Medica'
-            WHEN hr_alta IS NOT NULL THEN
-                'Liberacao Medica'
-        END situacao,
-        sn_fechada,
-        atendime.sn_retorno,
-        ds_ori_ate
-    FROM
-        dbamv.atendime
-        JOIN dbamv.paciente ON ( atendime.cd_paciente = paciente.cd_paciente )
-        JOIN dbamv.prestador ON ( atendime.cd_prestador = prestador.cd_prestador )
-        JOIN dbamv.convenio ON ( atendime.cd_convenio = convenio.cd_convenio )
-        JOIN tip_situacao ON ( tip_situacao.cd_tip_situacao = atendime.cd_tip_situacao )
-        LEFT JOIN evo_enf ON ( evo_enf.cd_atendimento = atendime.cd_atendimento )
-        LEFT JOIN vdic_recepcao_agenda ON ( vdic_recepcao_agenda.cd_atendimento = atendime.cd_atendimento )
-        LEFT JOIN item_agendamento ON ( item_agendamento.cd_item_agendamento = vdic_recepcao_agenda.cd_item_agendamento )
-        LEFT JOIN log_cid ON ( log_cid.cd_atendimento = atendime.cd_atendimento )
-        LEFT JOIN itreg_amb ON ( itreg_amb.cd_atendimento = atendime.cd_atendimento )
-        LEFT JOIN ori_ate ON ( ori_ate.cd_ori_ate = atendime.cd_ori_ate )
-        LEFT JOIN (
-            SELECT
-                atendime.cd_atendimento,
-                atendime.cd_paciente,
-                sacr_tempo_processo.dh_processo
-            FROM
-                atendime
-                JOIN sacr_tempo_processo ON ( sacr_tempo_processo.cd_atendimento = atendime.cd_atendimento )
-            WHERE
-                cd_tipo_tempo_processo = 31
-        ) sacr_tempo_processo ON ( sacr_tempo_processo.cd_atendimento = atendime.cd_atendimento )
-        LEFT JOIN (
-            SELECT
-                atendime.cd_atendimento,
-                atendime.cd_paciente,
-                sacr_tempo_processo.dh_processo AS hr_enf
-            FROM
-                atendime
-                JOIN sacr_tempo_processo ON ( sacr_tempo_processo.cd_atendimento = atendime.cd_atendimento )
-            WHERE
-                cd_tipo_tempo_processo = 30
-        ) sacr_tempo_processo_historico ON ( sacr_tempo_processo_historico.cd_atendimento = atendime.cd_atendimento )
-    WHERE
-        vdic_recepcao_agenda.cd_it_agenda_pai IS NULL
-    GROUP BY
-        atendime.cd_atendimento,
-        TO_CHAR(dt_atendimento, 'dd/mm/yyyy'),
-        TO_CHAR(hr_atendimento, 'hh24:mi'),
-        TO_CHAR(hr_enf, 'hh24:mi'),
-        paciente.nm_paciente,
-        nm_prestador,
-        nm_convenio,
-        DECODE(tp_agenda, 'A', 'Ambulatorio', 'E', 'Externo'),
-        /*
-        case
-            when ((vdic_recepcao_agenda.tp_agenda in ('A', 'I')) and (cd_item_agendamento in (19, 20, 21, 68, 69, 112, 113, 70, 86))) then 'PO'
-            when (vdic_recepcao_agenda.tp_agenda = 'I') then 'EXAME'
-            when (vdic_recepcao_agenda.tp_agenda = 'A') then 'AMBULATORIO'
-            else 'DESCONHECIDO'
-        end,
-        */
-        tip_situacao.ds_tip_situacao,
-        evo_enf.cd_evo_enf,
-        trunc(TO_CHAR(SYSDATE - paciente.dt_nascimento) / 365.25),
-        TO_CHAR(hr_evo_enf, 'hh24:mi'),
-        TO_CHAR(dh_processo, 'hh24:mi'),
-        TO_CHAR(hr_alta, 'hh24:mi'),
-        CASE
-                WHEN evo_enf.cd_evo_enf IS NOT NULL
-                     AND hr_alta IS NULL THEN
+            DT_ATENDIMENTO AS DATA,
+            ATENDIME.CD_ATENDIMENTO,
+            MIN(TO_CHAR(VDIC_RECEPCAO_AGENDA.HR_AGENDA, 'hh24:mi')) AS HR_AGENDA,
+            TO_CHAR(HR_ATENDIMENTO,'hh24:mi') HR_ATEND,
+            TO_CHAR(HR_ENF, 'hh24:mi') HR_ATO_ENF,
+            TO_CHAR(HR_EVO_ENF, 'hh24:mi') HR_EVOLUCAO,
+            MOD( TRUNC((MAX(HR_EVO_ENF - HR_ENF)) * 24), 24)
+            || ' : '
+            || MOD(TRUNC((MAX(HR_EVO_ENF - HR_ENF)) * 1440), 60 ) AS TP_ATO_ENF,
+            TO_CHAR(DH_PROCESSO, 'hh24:mi') HR_ATO_MED,
+            MOD(TRUNC((MAX(DH_PROCESSO - HR_EVO_ENF)) * 24), 24)
+            || ' : '
+            || MOD(TRUNC((MAX(DH_PROCESSO - HR_EVO_ENF)) * 1440), 60) AS TP_ENF_MED,
+            TO_CHAR(HR_ALTA, 'hh24:mi') HR_ALTA,
+            MOD(TRUNC((MAX(HR_ALTA - DH_PROCESSO)) * 24 ), 24)
+            || ' : '
+            || MOD(TRUNC((MAX(HR_ALTA - DH_PROCESSO)) * 1440), 60) AS TP_PACIENTE_SALA,
+            CASE
+                WHEN HR_ALTA IS NOT NULL THEN
+                    MOD(TRUNC((HR_ALTA - HR_ATENDIMENTO) * 24), 24)
+                    || ' : '
+                    || MOD(TRUNC((HR_ALTA - HR_ATENDIMENTO) * 1440), 60)
+                WHEN HR_ALTA IS NULL THEN
+                    MOD(TRUNC((SYSDATE - HR_ATENDIMENTO) * 24), 24)
+                    || ' : '
+                    || MOD(TRUNC((SYSDATE - HR_ATENDIMENTO) * 1440), 60)
+            END TP_TOTAL,
+            PACIENTE.NM_PACIENTE,
+            TRUNC(TO_CHAR(SYSDATE - PACIENTE.DT_NASCIMENTO) / 365.25) AS IDADE,
+            NM_PRESTADOR,
+            NM_CONVENIO,
+            TP_CONVENIO,
+            /* 15/05/2019 - MURILOCUNHA
+                   ACRESCENTADO TIPO "PO". COMO "PO" É UM ITEM DE AGENDAMENTO, 
+                   NAO DAVA PARA UTILIZAR SOMENTE A COLUNA TP_AGENDA, ENTAO,
+                   ADICIONOU-SE OUTRA COLUNA PARA COMPARACAO DE TIPO DE AGENDA
+                   
+                   DECODE(TP_AGENDA, 'A', 'Ambulatorio', 'E', 'Externo') AS TIPO,
+            */
+            CASE
+                WHEN (
+                       (VDIC_RECEPCAO_AGENDA.TP_AGENDA IN ('A', 'I')) AND 
+                       (VDIC_RECEPCAO_AGENDA.CD_ITEM_AGENDAMENTO IN (19, 20, 21, 68, 69, 112, 113, 70, 86)) AND
+                       (VDIC_RECEPCAO_AGENDA.HR_AGENDA IS NOT NULL)
+                     ) THEN 'PO'
+                WHEN (VDIC_RECEPCAO_AGENDA.TP_AGENDA = 'I') THEN 'EXAME'
+                WHEN (VDIC_RECEPCAO_AGENDA.TP_AGENDA = 'A') THEN 'AMBULATORIO'
+            END AS TIPO,
+            -- FIM - 15/05/2019 - MURILOCUNHA
+            CASE
+                WHEN EVO_ENF.CD_EVO_ENF IS NOT NULL
+                     AND HR_ALTA IS NULL THEN
                     'Liberado Atendimento'
-                WHEN hr_enf IS NOT NULL
-                     AND evo_enf.cd_evo_enf IS NULL THEN
+                WHEN HR_ENF IS NOT NULL
+                     AND EVO_ENF.CD_EVO_ENF IS NULL THEN
                     'Em Atendimento Enfermagem'
-                WHEN evo_enf.cd_evo_enf IS NOT NULL
-                     AND dh_processo IS NOT NULL THEN
+                WHEN EVO_ENF.CD_EVO_ENF IS NOT NULL
+                     AND DH_PROCESSO IS NOT NULL THEN
                     'Em Atendimento Medico'
-                WHEN evo_enf.cd_evo_enf IS NULL
-                     AND hr_alta IS NULL THEN
+                WHEN EVO_ENF.CD_EVO_ENF IS NULL
+                     AND HR_ALTA IS NULL THEN
                     'Aguardando Atendimento'
-                WHEN evo_enf.cd_evo_enf IS NULL
-                     AND hr_alta IS NOT NULL THEN
+                WHEN EVO_ENF.CD_EVO_ENF IS NULL
+                     AND HR_ALTA IS NOT NULL THEN
                     'Liberacao Medica'
-                WHEN hr_alta IS NOT NULL THEN
+                WHEN HR_ALTA IS NOT NULL THEN
                     'Liberacao Medica'
+            END SITUACAO,
+            SN_FECHADA,
+            ATENDIME.SN_RETORNO,
+            DS_ORI_ATE
+        FROM
+            DBAMV.ATENDIME
+            JOIN DBAMV.PACIENTE ON ( ATENDIME.CD_PACIENTE = PACIENTE.CD_PACIENTE )
+            JOIN DBAMV.PRESTADOR ON ( ATENDIME.CD_PRESTADOR = PRESTADOR.CD_PRESTADOR )
+            JOIN DBAMV.CONVENIO ON ( ATENDIME.CD_CONVENIO = CONVENIO.CD_CONVENIO )
+            JOIN TIP_SITUACAO ON ( TIP_SITUACAO.CD_TIP_SITUACAO = ATENDIME.CD_TIP_SITUACAO )
+            LEFT JOIN EVO_ENF ON ( EVO_ENF.CD_ATENDIMENTO = ATENDIME.CD_ATENDIMENTO )
+            LEFT JOIN VDIC_RECEPCAO_AGENDA ON ( VDIC_RECEPCAO_AGENDA.CD_ATENDIMENTO = ATENDIME.CD_ATENDIMENTO )
+            LEFT JOIN ITEM_AGENDAMENTO ON ( ITEM_AGENDAMENTO.CD_ITEM_AGENDAMENTO = VDIC_RECEPCAO_AGENDA.CD_ITEM_AGENDAMENTO )
+            LEFT JOIN LOG_CID ON ( LOG_CID.CD_ATENDIMENTO = ATENDIME.CD_ATENDIMENTO )
+            LEFT JOIN ITREG_AMB ON ( ITREG_AMB.CD_ATENDIMENTO = ATENDIME.CD_ATENDIMENTO )
+            LEFT JOIN ORI_ATE ON ( ORI_ATE.CD_ORI_ATE = ATENDIME.CD_ORI_ATE )
+            LEFT JOIN (
+                SELECT
+                    ATENDIME.CD_ATENDIMENTO,
+                    ATENDIME.CD_PACIENTE,
+                    SACR_TEMPO_PROCESSO.DH_PROCESSO
+                FROM
+                    ATENDIME
+                    JOIN SACR_TEMPO_PROCESSO ON ( SACR_TEMPO_PROCESSO.CD_ATENDIMENTO = ATENDIME.CD_ATENDIMENTO )
+                WHERE
+                    CD_TIPO_TEMPO_PROCESSO = 31
+            ) SACR_TEMPO_PROCESSO ON ( SACR_TEMPO_PROCESSO.CD_ATENDIMENTO = ATENDIME.CD_ATENDIMENTO )
+            LEFT JOIN (
+                SELECT
+                    ATENDIME.CD_ATENDIMENTO,
+                    ATENDIME.CD_PACIENTE,
+                    SACR_TEMPO_PROCESSO.DH_PROCESSO AS HR_ENF
+                FROM
+                    ATENDIME
+                    JOIN SACR_TEMPO_PROCESSO ON ( SACR_TEMPO_PROCESSO.CD_ATENDIMENTO = ATENDIME.CD_ATENDIMENTO )
+                WHERE
+                    CD_TIPO_TEMPO_PROCESSO = 30
+            ) SACR_TEMPO_PROCESSO_HISTORICO ON ( SACR_TEMPO_PROCESSO_HISTORICO.CD_ATENDIMENTO = ATENDIME.CD_ATENDIMENTO )
+        WHERE
+            VDIC_RECEPCAO_AGENDA.CD_IT_AGENDA_PAI IS NULL
+
+            --DESCOMENTAR APENAS PARA TESTES DE ALTERACAO NESTE SCRIPT
+            --AND DT_ATENDIMENTO = '16/may/2019'
+            --AND ATENDIME.CD_ATENDIMENTO = 15364
+
+        GROUP BY
+            TO_CHAR(DT_ATENDIMENTO, 'dd/mm/yyyy'),
+            ATENDIME.CD_ATENDIMENTO,
+            TO_CHAR(HR_ATENDIMENTO, 'hh24:mi'),
+            TO_CHAR(HR_ENF, 'hh24:mi'),
+            PACIENTE.NM_PACIENTE,
+            NM_PRESTADOR,
+            NM_CONVENIO,
+            --DECODE(TP_AGENDA, 'A', 'Ambulatorio', 'E', 'Externo'),
+            CASE
+                WHEN (
+                       (VDIC_RECEPCAO_AGENDA.TP_AGENDA IN ('A', 'I')) AND 
+                       (VDIC_RECEPCAO_AGENDA.CD_ITEM_AGENDAMENTO IN (19, 20, 21, 68, 69, 112, 113, 70, 86)) AND
+                       (VDIC_RECEPCAO_AGENDA.HR_AGENDA IS NOT NULL)
+                     ) THEN 'PO'
+                WHEN (VDIC_RECEPCAO_AGENDA.TP_AGENDA = 'I') THEN 'EXAME'
+                WHEN (VDIC_RECEPCAO_AGENDA.TP_AGENDA = 'A') THEN 'AMBULATORIO'
             END,
-        itreg_amb.sn_fechada,
-        tp_convenio,
-        CASE
-                WHEN hr_alta IS NOT NULL THEN
-                    mod(trunc((hr_alta - hr_atendimento) * 24), 24)
-                    || ' : '
-                    || mod(trunc((hr_alta - hr_atendimento) * 1440), 60)
-                WHEN hr_alta IS NULL THEN
-                    mod(trunc((SYSDATE - hr_atendimento) * 24), 24)
-                    || ' : '
-                    || mod(trunc((SYSDATE - hr_atendimento) * 1440), 60)
-            END,
-        dt_atendimento,
-        atendime.sn_retorno,
-        ds_ori_ate
-    ORDER BY
-        TO_CHAR(hr_atendimento, 'hh24:mi'),
-        cd_atendimento DESC;
+            TIP_SITUACAO.DS_TIP_SITUACAO,
+            EVO_ENF.CD_EVO_ENF,
+            TRUNC(TO_CHAR(SYSDATE - PACIENTE.DT_NASCIMENTO) / 365.25),
+            TO_CHAR(HR_EVO_ENF, 'hh24:mi'),
+            TO_CHAR(DH_PROCESSO, 'hh24:mi'),
+            TO_CHAR(HR_ALTA, 'hh24:mi'),
+            CASE
+                    WHEN EVO_ENF.CD_EVO_ENF IS NOT NULL
+                         AND HR_ALTA IS NULL THEN
+                        'Liberado Atendimento'
+                    WHEN HR_ENF IS NOT NULL
+                         AND EVO_ENF.CD_EVO_ENF IS NULL THEN
+                        'Em Atendimento Enfermagem'
+                    WHEN EVO_ENF.CD_EVO_ENF IS NOT NULL
+                         AND DH_PROCESSO IS NOT NULL THEN
+                        'Em Atendimento Medico'
+                    WHEN EVO_ENF.CD_EVO_ENF IS NULL
+                         AND HR_ALTA IS NULL THEN
+                        'Aguardando Atendimento'
+                    WHEN EVO_ENF.CD_EVO_ENF IS NULL
+                         AND HR_ALTA IS NOT NULL THEN
+                        'Liberacao Medica'
+                    WHEN HR_ALTA IS NOT NULL THEN
+                        'Liberacao Medica'
+                END,
+            ITREG_AMB.SN_FECHADA,
+            TP_CONVENIO,
+            CASE
+                    WHEN HR_ALTA IS NOT NULL THEN
+                        MOD(TRUNC((HR_ALTA - HR_ATENDIMENTO) * 24), 24)
+                        || ' : '
+                        || MOD(TRUNC((HR_ALTA - HR_ATENDIMENTO) * 1440), 60)
+                    WHEN HR_ALTA IS NULL THEN
+                        MOD(TRUNC((SYSDATE - HR_ATENDIMENTO) * 24), 24)
+                        || ' : '
+                        || MOD(TRUNC((SYSDATE - HR_ATENDIMENTO) * 1440), 60)
+                END,
+            DT_ATENDIMENTO,
+            ATENDIME.SN_RETORNO,
+            DS_ORI_ATE
+        ORDER BY
+            TO_CHAR(HR_ATENDIMENTO, 'hh24:mi'),
+            ATENDIME.CD_ATENDIMENTO DESC;
+
